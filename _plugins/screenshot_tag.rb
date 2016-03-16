42e426694d5a1e4b4a07c33143d4d28954ad795e
Hisashi HATAKEYAMA <me@hatak.jp>
2016-03-16 11:40:12 +0900

    fix plugin to remove lazyload setting because kibaco has compatibility setting

--- _plugins/screenshot_tag.rb
+++ _plugins/screenshot_tag.rb
@@ -22,7 +22,7 @@ module Jekyll
 
       s = %|<div class="thumbnail">\n|
       s += %|<a href="#{image_src}" data-toggle="lightbox" data-title="#{@description}">|
-      s += %|<img class="lazy img-responsive" data-original="#{image_src}">|
+      s += %|<img class="img-responsive" src="#{image_src}">|
       s += "</a>\n"
       s += %|<div class="caption">#{@description}</div>\n|
       s += "</div>\n"
