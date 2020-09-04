0caf6314d0364f154a91920d4bc38a0b66f4b06d
elearn2-std <69098309+elearn2-std@users.noreply.github.com>
2020-09-04 11:09:43 +0900

    Update q37.c

--- contents/c/basic/10/prog/q37.c
+++ contents/c/basic/10/prog/q37.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 void tasu_a(int x[], int a){
-    int c;
+    int c = 0;
     for(int i = 0;i<a;i++){
         c = c + x[i];
     }
@@ -9,7 +9,7 @@ void tasu_a(int x[], int a){
 }
 int main(void){
     int a;
-    printf("いくつ数字を入力ししますか？\n");
+    printf("いくつ数字を入力しますか？\n");
     scanf("%d",&a);
     int x[a];
     for(int i=0; i<a;i++){
