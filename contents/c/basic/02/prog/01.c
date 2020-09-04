e0bba8147db63716b4ec15a2010b02a21b648676
elearn2-std <69098309+elearn2-std@users.noreply.github.com>
2020-09-04 13:24:25 +0900

    Add files via upload

---
+++ contents/c/basic/02/prog/01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main() {
+    int x;
+    int a;
+    int b;
+    a = 5;
+    b = 2;
+    x = a + b;
+    printf("%d\n", x);
+    x = a - b;
+    printf("%d\n", x); 
+    x = a * b;
+    printf("%d\n", x);
+    x = a / b;
+    printf("%d\n", x);
+    x = a % b;
+    printf("%d\n", x);
+}
